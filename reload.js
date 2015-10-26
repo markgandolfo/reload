38dacfdaaa2f023c7ce00c368ce6e367029e9a68
Mark Gandolfo <mark@gandolfo.com.au>
2015-10-26 11:56:38 +1100

    added unloading, and improved readme

--- reload.js
+++ reload.js
@@ -1,5 +1,7 @@
+console.info("reload.js is now listening for SHIFT + R to reload the css files");
 window.addEventListener("keydown", function(e) {
   if(e.shiftKey && e.keyCode == 82) {
+    console.info("reload.js: Reloading css files");
     var links = document.getElementsByTagName('link');
     for (var i = 0; i<links.length; i++) {
       var otac = links[i].parentElement;
@@ -7,6 +9,11 @@ window.addEventListener("keydown", function(e) {
       otac.removeChild(klinac);
       otac.appendChild(klinac);
     }
+  } else if(e.shiftKey && e.keyCode == 85) {
+    console.info("reload.js: Unloading reload.js, to re-initialize, you'll need to reload the plugin");
+    var el = document.getElementById('reload.js');
+    el.parentNode.removeChild(el);
   }
+
 });
 
